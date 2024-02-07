6835a49e1359885aa0056c72aec4db28d61e7056
FlqmeJP <135798217+FlqmeJP@users.noreply.github.com>
2024-02-07 16:56:04 +0900

    Update clickLeft.py

--- python/minecraft/clickLeft.py
+++ python/minecraft/clickLeft.py
@@ -3,10 +3,9 @@ import threading
 from pynput.mouse import Controller, Button
 from pynput.keyboard import Listener, KeyCode, Controller as KeyboardController
 
-TOGGLE_KEY = KeyCode(char="t")　# プログラムの有効、無効を設定
-QUICK_ATTACK_KEY = KeyCode(char="q") # 攻撃するキー
+QUICK_ATTACK_KEY = KeyCode(char="q")  # 攻撃するキー
 
-clicking = False
+clicking = True  # プログラム開始時に有効にする
 mouse = Controller()
 keyboard = KeyboardController()
 
@@ -18,13 +17,7 @@ def clicker():
             time.sleep(0.01)  # 必要に応じてこの遅延を調整してください
             keyboard.release(QUICK_ATTACK_KEY)
 
-def toggle_event(key):
-    if key == TOGGLE_KEY:
-        global clicking
-        clicking = not clicking
-
 click_thread = threading.Thread(target=clicker)
 click_thread.start()
 
-with Listener(on_press=toggle_event) as listener:
-    listener.join()
+click_thread.join()
