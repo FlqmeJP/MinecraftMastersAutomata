483def3f959e563039d4461731510e8d9d7f461a
72q84g <135799349+72q84g@users.noreply.github.com>
2024-02-10 14:45:35 +0900

    Add files via upload

--- judge.c
+++ judge.c
@@ -2,6 +2,11 @@
 #include <stdio.h>
 #include <unistd.h>
 
+int zombie1(int z1[]);
+int zombie2(long int z2[]);
+void z1print(int z1[]);
+void z2print(long int z2[]);
+
 int zombie1(int z1[]){
     int a,i,k=1;
     int flag = 0;
@@ -13,6 +18,7 @@ int zombie1(int z1[]){
     }
     k = 1;
     if(a==0){flag=1;}
+    z1print(z1);
     return flag;
 }
 
@@ -28,6 +34,8 @@ int zombie2(long int z2[]){
         k = k * 10;
     }
     k = 1;
+
+    if(b==0){flag=1;}
     for(i=0;i<15;i++){
         if(z2[i]!=0){
             if(z2[i]!=1){
@@ -35,7 +43,37 @@ int zombie2(long int z2[]){
             }
         }
     }
+    z2print(z2);
     return flag;
+}
+
+void z1print(int z1[]){
+    if(z1[0]==1){printf("左上 ");}
+    if(z1[1]==1){printf("中央上 ");}
+    if(z1[2]==1){printf("右上 ");}
+    if(z1[3]==1){printf("左下 ");}
+    if(z1[4]==1){printf("中央下 ");}
+    if(z1[5]==1){printf("右下 ");}
+    printf("\n");
+}
+
+void z2print(long int z2[]){
+    if(z2[0]==1){printf("右下遠 ");}
+    if(z2[1]==1){printf("右下中 ");}
+    if(z2[2]==1){printf("右下近 ");}
+    if(z2[3]==1){printf("左下遠 ");}
+    if(z2[4]==1){printf("左下中 ");}
+    if(z2[5]==1){printf("左下近 ");}
+    if(z2[6]==1){printf("右上遠 ");}
+    if(z2[7]==1){printf("右上中 ");}
+    if(z2[8]==1){printf("右上近 ");}
+    if(z2[9]==1){printf("左上遠 ");}
+    if(z2[10]==1){printf("左上中 ");}
+    if(z2[11]==1){printf("左上近 ");}
+    if(z2[12]==1){printf("中央遠 ");}
+    if(z2[13]==1){printf("中央中 ");}
+    if(z2[14]==1){printf("中央近 ");}
+    printf("\n");
 }
 
 int main(void) {
@@ -52,38 +90,9 @@ int main(void) {
         int z1[7];
         long int z2[15];
 
-        printf("zombie1\n");
-        zombie1(z1);
-        if(z1[0]==1){printf("左上 ");}
-        if(z1[1]==1){printf("中央上 ");}
-        if(z1[2]==1){printf("右上 ");}
-        if(z1[3]==1){printf("左下 ");}
-        if(z1[4]==1){printf("中央下 ");}
-        if(z1[5]==1){printf("右下 ");}
+        zombie2(z2);
         printf("\n");
 
-        printf("zombie2\n");
-        flag = zombie2(z2);//数字がバグっていたらflag=1になる。正常値ならflag=0;
-        if(flag==0){
-            if(z2[0]==1){printf("右下遠 ");}
-            if(z2[1]==1){printf("右下中 ");}
-            if(z2[2]==1){printf("右下近 ");}
-            if(z2[3]==1){printf("左下遠 ");}
-            if(z2[4]==1){printf("左下中 ");}
-            if(z2[5]==1){printf("左下近 ");}
-            if(z2[6]==1){printf("右上遠 ");}
-            if(z2[7]==1){printf("右上中 ");}
-            if(z2[8]==1){printf("右上近 ");}
-            if(z2[9]==1){printf("左上遠 ");}
-            if(z2[10]==1){printf("左上中 ");}
-            if(z2[11]==1){printf("左上近 ");}
-            if(z2[12]==1){printf("中央遠 ");}
-            if(z2[13]==1){printf("中央中 ");}
-            if(z2[14]==1){printf("中央近 ");}
-        }
-        else{printf("error");}
-        printf("\n");
-        
         sleep(1);
     }
     return 0;
